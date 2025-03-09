e9be31c02dc543c2289bbc9a6dc8d175d38fa2d0
vova <ghdfkr@ffgg.com>
2025-03-09 13:26:25 +0200

    kfds

--- maze.py
+++ maze.py
@@ -15,8 +15,8 @@ pygame.display.set_caption("Лабіринт. Автор: ....")
 clock = pygame.time.Clock()
 
 pygame.mixer.init()
-pygame.mixer.music.load("jungles.ogg")
-pygame.mixer.music.play()
+#pygame.mixer.music.load("jungles.ogg")
+#pygame.mixer.music.play()
 
 class GameSprite(pygame.sprite.Sprite) :
     def __init__ (self, filename:str, size:tuple[int,int], coords: tuple[int,int], speed:int):
@@ -49,6 +49,14 @@ class Enemy(GameSprite):
         if self.rect.x >= x2 or self.rect.x <= x1:
             self.speed = -self.speed
 
+class wall:
+    def __init__(self, coords:tuple[int,int], size:tuple[int,int], color:tuple[int,int,int]):
+        self.rect = pygame.Rect(coords, size)
+        self.color = color
+
+    def draw(self, windiw:pygame.Surface, width=0):
+        pygame.draw.rect(window, self.color, self.rect, width=width)
+        
 
         
 
@@ -58,19 +66,67 @@ class Enemy(GameSprite):
 player = Player("hero.png",(75,75), (50, HEIGHT-50), 5)
 enemy = Enemy("cyborg.png",(75,75), (WIDTH-250, HEIGHT//2), 5)
 gold = GameSprite("treasure.png",(75,75), (WIDTH-150, HEIGHT-50), 5)
+
+wall_0 = wall((0,0),(WIDTH, HEIGHT),(10,240,10))
+walls = [
+    wall((10,10), (300,10), (50,200,50)),
+    wall((300,10), (10,300), (50,200,50)),
+    wall((10,300), (100,10), (50,200,50)),
+    wall((10, 588), (400,10), (50,200,50)),
+    wall((530, 124), (10, 1700), (50,200,50)),
+    wall((402, 517), (10,80), (50,200,50))
+]
 game = True
+finish = False
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.pos)
+    if not finish:
+        window.blit(backgound, (0,0))
+        player.update()
+        player.reset(window)
+        enemy.update(700, WIDTH-200)
+        enemy.reset(window)
 
+        gold.reset(window)
 
-    window.blit(backgound, (0,0))
-    player.update()
-    player.reset(window)
-    enemy.update(700, WIDTH-200)
-    enemy.reset(window)
-    gold.reset(window)
+        if pygame.sprite.collide_rect(player, enemy):
+            finish = True
+            kick = pygame.mixer.Sound("kick.ogg")
+            kick.play()
+
+            pygame.font.init()
+            font1 = pygame.font.Font(None,48)
+            text = font1.render("YOU DIED", True, (255,0,0))
+            window.blit(text, (WIDTH//2-100, HEIGHT//2))
+
+        if pygame.sprite.collide_rect(player, gold):
+            finish = True
+            kick = pygame.mixer.Sound("money.ogg")
+            kick.play()
+
+            pygame.font.init()
+            font1 = pygame.font.Font(None,48)
+            text = font1.render("YOU WIN", True, (255,0,0))
+            window.blit(text, (WIDTH//2-100, HEIGHT//2))
+        
+        
+
+        wall_0.draw(window, width=10)
+        for w in walls:
+            w.draw(window)
+            if pygame.sprite.collide_rect(player, w):
+                finish = True
+                kick = pygame.mixer.Sound("kick.ogg")
+                kick.play()
+
+                pygame.font.init()
+                font1 = pygame.font.Font(None,48)
+                text = font1.render("YOU DIED", True, (255,0,0))
+                window.blit(text, (WIDTH//2-100, HEIGHT//2))
 
     pygame.display.update()
     clock.tick(FPS)
